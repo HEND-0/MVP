c2dd19278025d068ae46f60ea9feb84ec54ae9a0
WooWan <woohobi@gmail.com>
2020-09-06 15:06:51 +0900

    main페이지 작업

---
+++ HEND/main/static/js/hamburg.js
@@ -0,0 +1,4 @@
+const hamburg1=document.querySelector(".hamburg_ele1");
+const hamburg1 = document.querySelector(".hamburg_ele2");
+const hamburg1 = document.querySelector(".hamburg_ele3");
+
---
+++ HEND/main/static/js/scrollto.js
@@ -0,0 +1,17 @@
+const panelBtn1 = document.querySelector(".panelBtn1");
+const panelBtn2 = document.querySelector(".panelBtn2");
+const panelBtn3 = document.querySelector(".panelBtn3");
+const part1 = document.querySelector(".content_back1");
+const part2 = document.querySelector(".content_back2");
+const part3 = document.querySelector(".content_back3");
+
+panelBtn1.addEventListener("click", () => {
+  part1.scrollIntoView({ behavior: "smooth" });
+});
+panelBtn2.addEventListener("click", () => {
+  part2.scrollIntoView({ behavior: "smooth" });
+});
+panelBtn3.addEventListener("click", () => {
+  part3.scrollIntoView({ behavior: "smooth" });
+});
+console.log("sdsd");
